7c537b97f0e10711ca6db82416ac5152171d8c09
mksbot <81396341+mksbot@users.noreply.github.com>
2023-12-15 10:56:44 -0300

    all

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from pprint import pprint
 import telebot
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+
 from selenium.webdriver.common.by import By
-import os
+
 import time
 
 from arquivos_texto import registro, abrir_reg
@@ -11,33 +11,33 @@ from arquivos_texto import registro, abrir_reg
 API_TOKEN = '6159093978:AAEyVQZYRBA2YYkX6GNwl9ypGBWHYGUwNz4'
 
 bot = telebot.TeleBot(API_TOKEN)
-page = ('https://www.crunchyroll.com/pt-br/simulcastcalendar?filter=premium&date=2023-12-11')
-service = Service()
-optiones = webdriver.ChromeOptions()
-driver = webdriver.Chrome(service=service, options=optiones)
-while True:
-    time.sleep(500)
+page = ('https://animesonline.nz/episodio')
+
+driver = webdriver.Firefox()
+if 1 == 1:
+    time.sleep(1)
     driver.get(page)
     final = None
     titulo2 = []
-    titulo = driver.find_elements(By.TAG_NAME,'article')
+    titulo = driver.find_elements(By.TAG_NAME, 'h3')
+    print(titulo[5].text)
 
-    imagen = driver.find_elements(By.TAG_NAME,'a')
-    for nu, it in enumerate(imagen):
-        if 'JUJUTSU KAISEN SEASON 2'.lower().replace(' ', '-') in str(it.get_attribute('href')):
-            print(it.get_attribute('href'))
-    for nu, it in enumerate(titulo[-10:]):
-        texto = f'{it.text}'
-        try:
-            reg = abrir_reg('animes')
-        except:
-            registro('', 'animes', 'nao')
-            reg = abrir_reg('animes')
-        if texto not in reg:
-            if 'pm' in it.text or 'am' in it.text:
-                registro(texto, 'animes', 'nao')
-                time.sleep(6)
-                bot.send_message(6512880606, f'🧿 {texto}')
+    # imagen = driver.find_elements(By.TAG_NAME,'a')
+    # for nu, it in enumerate(imagen):
+    #     if 'JUJUTSU KAISEN SEASON 2'.lower().replace(' ', '-') in str(it.get_attribute('href')):
+    #         print(it.get_attribute('href'))
+    # for nu, it in enumerate(titulo[-10:]):
+    #     texto = f'{it.text}'
+    #     try:
+    #         reg = abrir_reg('animes')
+    #     except:
+    #         registro('', 'animes', 'nao')
+    #         reg = abrir_reg('animes')
+    #     if texto not in reg:
+    #         if 'pm' in it.text or 'am' in it.text:
+    #             registro(texto, 'animes', 'nao')
+    #             time.sleep(6)
+    #             bot.send_message(6512880606, f'🧿 {texto}')
 
     #
 
